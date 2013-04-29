6b2af5dfa7a991eaaa9bad5516d1f26cdb19b583
Arpad Borsos <arpad.borsos@googlemail.com>
2013-04-29 20:34:59 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,16 +1,35 @@
-# 
+# walker, texas ranger
 
+very simple walker for esprima AST
 
-[![Build Status](https://travis-ci.org/Swatinem/.png?branch=master)](https://travis-ci.org/Swatinem/)
-[![Coverage Status](https://coveralls.io/repos/Swatinem//badge.png?branch=master)](https://coveralls.io/r/Swatinem/)
-[![Dependency Status](https://gemnasium.com/Swatinem/.png)](https://gemnasium.com/Swatinem/)
+[![Build Status](https://travis-ci.org/Swatinem/eswalker.png?branch=master)](https://travis-ci.org/Swatinem/eswalker)
+[![Coverage Status](https://coveralls.io/repos/Swatinem/eswalker/badge.png?branch=master)](https://coveralls.io/r/Swatinem/eswalker)
+[![Dependency Status](https://gemnasium.com/Swatinem/eswalker.png)](https://gemnasium.com/Swatinem/eswalker)
 
 ## Installation
 
-    $ npm install 
+    $ npm install eswalker
 
 ## Usage
 
+```js
+	walker(esprima.parse("…"), {
+		MemberExpression: function (recurse, stop) {
+			// the node can be used as `this`
+			this.object;
+			// you are responsible to call `recurse()` on all the children yourself
+			recurse(this.object);
+			recurse(this.property);
+		},
+		default: function (recurse, stop) {
+			// call or throw `stop` to completely stop walking.
+			stop();
+			throw stop;
+		}
+	}, offset);
+	// when offset is set, will only recurse to nodes that lie within the offset
+	// esprima option {range: true} needs to be set for this to work
+```
 
 ## License
 
--- test/test.js
+++ test/test.js
@@ -1,5 +1,114 @@
-var  = require('../');
+var walker = require('../');
 
-describe('', function () {
-
+describe('walker', function () {
+	it('should call the function corresponding to the type', function () {
+		var obj = {
+			type: 'Type'
+		};
+		var called = false;
+		walker(obj, {
+			Type: function () {
+				called = true;
+				obj.should.equal(this);
+			}
+		});
+		called.should.eql(true);
+	});
+	it('should call the `default` function on unknown types', function () {
+		var obj = {
+			type: 'Type'
+		};
+		var called = false;
+		walker(obj, {
+			default: function () {
+				called = true;
+				obj.should.equal(this);
+			}
+		});
+		called.should.eql(true);
+	});
+	it('should handle arrays', function () {
+		var obj = {
+			type: 'Type',
+			arr: [{
+				type: 'Type2'
+			}, {
+				type: 'Type2'
+			}, {
+				type: 'Type2'
+			}]
+		};
+		var called = 0;
+		walker(obj, {
+			Type2: function () {
+				called++;
+			}
+		});
+		called.should.eql(3);
+	});
+	it('should require objects to have `type`', function () {
+		var fnTable = {
+			Type: function (recurse) {
+				recurse(this.child);
+			},
+			Type2: function () {
+				walker.checkProps(this.child);
+			},
+			default: function () {
+				should.fail('should not be reached');
+			}
+		}
+		walker({no: 'Type'}, fnTable);
+		walker({
+			type: 'Type',
+			child: {
+				no: 'Type'
+			}
+		}, fnTable);
+		walker({
+			type: 'Type2',
+			child: {
+				no: 'Type'
+			}
+		}, fnTable);
+	});
+	it('should stop()', function () {
+		walker({
+			type: 'Type1',
+			prop: {
+				type: 'Type2'
+			}
+		}, {
+			Type1: function (recurse, stop) {
+				stop();
+			},
+			Type2: function () {
+				should.fail('should not be reached');
+			}
+		});
+	});
+	it('should only recurse when the node is in range', function () {
+		walker({
+			type: 'Type1',
+			prop: {
+				type: 'Type2',
+				range: [0,5]
+			}
+		}, {
+			Type2: function () {
+				should.fail('should not be reached');
+			}
+		}, 10);
+	});
+	it('should rethrow exceptions', function () {
+		(function () {
+		walker({
+			type: 'Type1'
+		}, {
+			Type1: function () {
+				throw 1;
+			}
+		});
+		}).should.throw();
+	});
 });
